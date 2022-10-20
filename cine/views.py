29177e3934155ee71501ed07be7043b963b07c9d
danieltm1987 <daniel.tm1987@gmail.com>
2022-10-20 18:57:09 -0500

    Se agregan vistas y Urls

--- cine/views.py
+++ cine/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from cine.serializers import *
+from cine.models import *
 
-# Create your views here.
+
+class Cine_view(viewsets.ModelViewSet):
+    queryset = Cine.objects.all()
+    serializers_class = Cine_serializer
+
+
+class Sala_view(viewsets.ModelViewSet):
+    queryset = Sala.objects.all()
+    serializers_class = Sala_serializer
+
+
+class Pelicula_view(viewsets.ModelViewSet):
+    queryset = Pelicula.objects.all()
+    serializers_class = Pelicula_serializer
+
+
+class Funcion_view(viewsets.ModelViewSet):
+    queryset = Funcion.objects.all()
+    serializers_class = Funcion_serializer
+
+
+class Boleta_view(viewsets.ModelViewSet):
+    queryset = Boleta.objects.all()
+    serializers_class = Boleta_serializer
+
--- cine_grupo55/urls.py
+++ cine_grupo55/urls.py
@@ -1,21 +1,15 @@
-"""cine_grupo55 URL Configuration
+from django.urls import path, include
+from rest_framework import routers
+from cine.views import *
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-# from django.contrib import admin
-from django.urls import path
+router = routers.DefaultRouter()
+router.register('cine',Cine_view,basename='cine')
+router.register('sala',Sala_view,basename='sala')
+router.register('pelicula',Pelicula_view,basename='pelicula')
+router.register('funcion',Funcion_view,basename='funcion')
+router.register('boleta',Boleta_view,basename='boleta')
+
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+    path('',include(router.urls))
 ]
